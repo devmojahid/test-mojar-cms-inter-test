eb79fb880e294c7cecaf9c03b3d45b086f3a11db
Mojahid islam <raofahmedmojahid@gmail.com>
2024-06-04 23:29:43 +0600

    global

--- Modules/Core/app/Contracts/GlobalDataContract.php
+++ Modules/Core/app/Contracts/GlobalDataContract.php
@@ -1,8 +1,11 @@
-<?php 
+<?php
+
 namespace Modules\Core\Contracts;
 
-interface GlobalDataContract{
-    public function set(string $key, string|array $value = null): void;
+interface GlobalDataContract
+{
+    public function set(string $key, string|array $value);
+    public function push(string $key, string|array $value): void;
     public function get(string $key, string|array $default = null): string|array;
     public function all(): array;
 }
--- Modules/Core/app/Supports/GlobalData.php
+++ Modules/Core/app/Supports/GlobalData.php
@@ -2,6 +2,32 @@
 
 namespace Modules\Core\Supports;
 
-class GlobalData
+use Modules\Core\Contracts\GlobalDataContract;
+use Illuminate\Support\Arr;
+
+class GlobalData implements GlobalDataContract
 {
+    protected array $values = [];
+
+    public function set(string $key, string|array $value): void
+    {
+        Arr::set($this->values, $key, $value);
+    }
+
+    public function push(string $key, $value): void
+    {
+        $data = $this->get($key, []);
+        $data[] = $value;
+        $this->set($key, $data);
+    }
+
+    public function get(string $key, $default = null): string|array
+    {
+        return Arr::get($this->values, $key, $default);
+    }
+
+    public function all(): array
+    {
+        return $this->values;
+    }
 }
